b940ec312567df1f7fc561b1c1d92faeff736a9b
Himadri Bhargava <himadribhargava@gmail.com>
2019-06-11 20:04:02 +0530

    added server file for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log("server is listening at port 8080");
+});
